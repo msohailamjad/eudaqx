2c4329532051aacff5c050626000e070067f195b
Mohammed Sohail Amjad <msamjad@pc172.hep.ucl.ac.uk>
2017-02-10 12:57:33 +0000

    eudaq/gui. Imported some improvements from eudaq-1.7 gui to eudaq2.

--- gui/include/RunControlModel.hh
+++ gui/include/RunControlModel.hh
@@ -53,6 +53,7 @@ public:
 
   int GetLevel(const QModelIndex &index) const;
   void UpdateDisplayed();
+  bool CheckConfigured();
 
   int rowCount(const QModelIndex &parent = QModelIndex()) const;
   int columnCount(const QModelIndex &parent = QModelIndex()) const;
